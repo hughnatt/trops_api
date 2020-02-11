235bd44e8e58891165d619f5eb9c79010959404b
qsibue <quentin.sibue@live.fr>
2020-02-11 16:05:55 +0100

    handle user token

--- src/routers/api.js
+++ src/routers/api.js
@@ -120,28 +120,30 @@ router.post('/advert', async (req, res) => {
 
 router.get('/advert',async (req,res) => {
     try{
-        Advert.find(function(err, advert){
+        Advert.find().sort({'creationDate' : -1}).exec(function(err, advert){
             if (err){
                 res.status(400).send(err); 
             }
             res.json(advert);   
-        });            
+        })
     }
     catch(error){
         res.status(400).send({error : error.message})
     }
 })
 
-router.get('/advert/owner',async (req,res) => { //TODO : Handle owner Token !!
+router.get('/advert/owner', auth, async (req,res) => { 
     try{
-        var query = Advert.find({});
-        query.where('owner', req.body.owner);
-        query.exec(function (err, results) {
-            if (err){
-                res.status(400).send(err); 
-            }
-            res.json(results); 
-        });
+        if(req.user.email == req.body.owner){ //prevent anyone to get the advert of a given user
+            var query = Advert.find({});
+            query.where('owner', req.body.owner);
+            query.exec(function (err, results) {
+                if (err){
+                    res.status(400).send(err); 
+                }
+                res.json(results); 
+            });
+        }
     }
     catch{
         res.status(400).send({error : error.message})
@@ -165,15 +167,26 @@ router.get('/advert/category',async (req,res) => {
 })
 
 
-router.delete('/advert/delete',async (req,res) => { //TODO : Handle the user's token !!
+router.delete('/advert/delete', auth, async (req,res) => {
     try{
-        Advert.findOneAndRemove({_id: req.body._id},function(err,data)
-        {
-            if(err){
+        var query = Advert.find({}); // querry to get the advert where his id equal the one in body
+        query.where('_id', req.body._id);
+        query.exec(function (err, results) {
+            if (err){
                 res.status(400).send(err); 
             }
+            else if(req.user.email == results[0].owner){ //once the good advert get, deletion only if current user mail == owner mail to prevent abusive deletion
+                Advert.deleteOne({ '_id': req.body._id}, function (err) {
+                    if(err){
+                        res.status(400).send(err); 
+                    }
+                })
+            }
+            else{
+                res.status(401).send(err); 
+            }
+            res.status(204).send({message : "Advert deleted with success"}); 
         });
-        res.status(204).send({message : "Advert deleted with success"}); 
     }
     catch{
         res.status(400).send({error : error.message})
