956cd7323dd91c9944f7bc94f7b431b11423630f
Hugh Natt <hughnatt@protonmail.com>
2020-02-14 14:46:34 +0100

    Setup Category for Tree structure

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
+const tree = require('../lib/tree')
 
 const categorySchema = mongoose.Schema({
-
-    categoryName : {
+    name: {
         type: String,
         required: true,
         trim: true
     }
-})
+});
+categorySchema.plugin(tree);
 
-const Category = mongoose.model('Category', categorySchema)
+
+const Category = mongoose.model('Category', categorySchema);
 
 module.exports = Category
