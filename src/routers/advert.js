a828db7302c762f5e3710644b7f5100189359319
qsibue <quentin.sibue@live.fr>
2020-02-20 10:10:29 +0100

    add update advert endpoint

--- src/routers/advert.js
+++ src/routers/advert.js
@@ -29,16 +29,16 @@ router.get('/advert',async (req,res) => {
     }
 })
 
-router.delete('/advert', auth, async (req,res) => {
+router.delete('/advert/:id', auth, async (req,res) => {
     try{
         var query = Advert.find({}); // querry to get the advert where his id equal the one in body
-        query.where('_id', req.body._id);
+        query.where('_id', req.params.id);
         query.exec(function (err, results) {
             if (err){
                 res.status(400).send(err); 
             }
             else if(req.user.email == results[0].owner){ //once the good advert get, deletion only if current user mail == owner mail to prevent abusive deletion
-                Advert.deleteOne({ '_id': req.body._id}, function (err) {
+                Advert.deleteOne({ '_id': req.params.id}, function (err) {
                     if(err){
                         res.status(400).send(err); 
                     }
@@ -66,6 +66,37 @@ router.post('/advert/owner', auth, async (req,res) => {
                     res.status(400).send(err); 
                 }
                 res.json(results); 
+            });
+        }
+    }
+    catch{
+        res.status(400).send({error : error.message})
+    }
+})
+
+router.put('/advert/:id',auth,async(req,res) => {
+    try{
+
+        var query = Advert.find({}); // querry to get the advert where his id equal the one in body
+        query.where('_id', req.params.id);
+        query.exec(function (err, results) {
+
+
+
+            
+        })
+
+        if(req.user.email == req.body.owner){ //prevent anyone to get the advert of a given user
+            
+            const filter = { _id: req.params.advertID };
+        
+
+            Advert.findOneAndUpdate({filter}, req.body,{new : true}, function(err, result) {
+                if (err) {
+                  res.status(400).send(err);
+                } else {
+                  res.status(200).send();
+                }
             });
         }
     }
