f70992171e066bb786cb8dbd05ac53cebbecb55d
Hugh Natt <hughnatt@protonmail.com>
2020-03-05 11:04:10 +0100

    Add admin authentication directly in auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,37 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const Admin = require('../models/Admin')
 
 const auth = async(req, res, next) => {
     try {
+
         const token = req.header('Authorization').replace('Bearer ', '')
-        
-        const data = jwt.verify(token, process.env.JWT_KEY)
-    
-        const user = await User.findOne({ _id: data._id, 'tokens.token': token })
-        
-        if (!user) {
-            throw new Error()
-        }
-        req.user = user
         req.token = token
+
+        let data;
+        try {
+            data = jwt.verify(token, process.env.JWT_KEY)
+            const user = await User.findOne({ _id: data._id, 'tokens.token': token })
+    
+            if (user) {
+                req.user = user       
+                req.isAdmin = false     
+            } else {
+
+            }
+        } catch (error){
+            data = jwt.verify(token, process.env.JWT_ADMIN_KEY)
+            const admin = await Admin.findOne({_id : data._id, 'tokens.token': token})
+            if (admin){
+                req.admin = admin
+                req.isAdmin = true
+            } else {
+                throw new Error()
+            }
+        }
         next()
     } catch (error) {
+        console.log(error)
         res.status(401).send({ error: 'Not authorized to access this resource' })
     }
 
